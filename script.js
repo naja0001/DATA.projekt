ffb57f91fe10c3ed2ad85fb15bb6b8c4065af77a
naja0001 <Naima_3108@hotmail.com>
2023-03-30 09:16:12 +0200

    Changes

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
-  const pokemons = await getPokemon("data/pokemon.JSON");
+  const pokemons = await getPokemon(
+    "https://cederdorff.github.io/dat-js/05-data/pokemons.json"
+  );
 
   //for of loop pokemon et object af pokemons array'et
   for (const pokemon of pokemons) {
@@ -24,23 +26,15 @@ function showpokemon(pokemons) {
   console.log(pokemons);
 
   const myPokemons = /* html */ `
-  <!--
-<li> <b> Disccription</b>: <br> ${pokemons.discription}</li>
-  <br><li> <b>Name</b>: ${pokemons.name}</li>
-  <li> <b>Number</b>: ${pokemons.number} </li>
-  <li> <b>Height</b>: ${pokemons.height} </li>
-  <li> <b>Weight</b>: ${pokemons.weight} </li>
-  <li> <b>Category</b>: ${pokemons.category} </li>
-  <li> <b>Gender</b>: ${pokemons.gender} </li>
-  <li> <b>Type</b>: ${pokemons.type} </li>
-  <li> <b>Weaknesses</b>: ${pokemons.weaknesses} </li>-->
   
     <article class="grid-item">
-        <h2>${pokemons.name}</h2>
+        <h2>${
+          pokemons.name[0].toUpperCase() + pokemons.name.slice(1).toLowerCase()
+        }</h2>
         <img src = "${pokemons.image}" alt=""/>
-        <p>Ability: ${pokemons.ability}</p>
-        <p> Type: ${pokemons.type}</p>
-        <p> Gender: ${pokemons.gender}</p>
+        <h4>Type: ${
+          pokemons.type[0].toUpperCase() + pokemons.type.slice(1).toLowerCase()
+        }</h4>
     </article>
 
 `;
@@ -60,61 +54,33 @@ function showpokemon(pokemons) {
     document.querySelector("#detail-info").textContent = pokemons.name;
     document.querySelector("#detail-description").textContent =
       pokemons.description;
-    document.querySelector(
-      "#detail-number"
-    ).textContent = `Number: ${pokemons.number}`;
-    document.querySelector(
-      "#detail-category"
-    ).textContent = `Category: ${pokemons.category}`;
-    document.querySelector(
-      "#detail-ability"
-    ).textContent = `Ability: ${pokemons.ability}`;
-    document.querySelector(
-      "#detail-dexindex"
-    ).textContent = `Dexindex: ${pokemons.dexindex}`;
-    document.querySelector(
-      "#detail-subtype"
-    ).textContent = `Subtype: ${pokemons.subtype}`;
-    document.querySelector(
-      "#detail-generation"
-    ).textContent = `Generation: ${pokemons.generation}`;
-    document.querySelector(
-      "#detail-height"
-    ).textContent = `Height: ${pokemons.height} cm`;
-    document.querySelector(
-      "#detail-weight"
-    ).textContent = `Weight: ${pokemons.weight} kg`;
-    document.querySelector(
-      "#detail-gender"
-    ).textContent = `Gender: ${pokemons.gender}`;
-    document.querySelector(
-      "#detail-type"
-    ).textContent = `Type: ${pokemons.type}`;
-    document.querySelector(
-      "#detail-weaknesses"
-    ).textContent = `Weaknesses: ${pokemons.weaknesses}`;
-    document.querySelector(
-      "#detail-spilversion"
-    ).textContent = `Spilversion: ${pokemons.spilversion}`;
+    document.querySelector("#detail-category").textContent = pokemons.category;
+    document.querySelector("#detail-ability").textContent = pokemons.ability;
+    document.querySelector("#detail-dexindex").textContent = pokemons.dexindex;
+    document.querySelector("#detail-subtype").textContent = pokemons.subtype;
+    document.querySelector("#detail-generation").textContent =
+      pokemons.generation;
+    document.querySelector("#detail-height").textContent = pokemons.height;
+    document.querySelector("#detail-weight").textContent = pokemons.weight;
+    document.querySelector("#detail-gender").textContent = pokemons.gender;
+    document.querySelector("#detail-type").textContent = pokemons.type;
+    document.querySelector("#detail-weaknesses").textContent =
+      pokemons.weaknesses;
+    document.querySelector("#detail-spilversion").textContent =
+      pokemons.spilversion;
+    document.querySelector("#detail-statsHP").textContent = pokemons.statsHP;
+    document.querySelector("#detail-statsattack").textContent =
+      pokemons.statsAttack;
+    document.querySelector("#detail-statsDefense").textContent =
+      pokemons.statsDefence;
+    document.querySelector("#detail-statsSpecielAttack").textContent =
+      pokemons.statsSpecielAttack;
+    document.querySelector("#detail-statsSpecielDefense").textContent =
+      pokemons.statsSpecielDefence;
 
     let canEvolve = generateEvolve(pokemons);
 
     document.querySelector("#detail-canEvolve").textContent = canEvolve;
-    document.querySelector(
-      "#detail-statsHP"
-    ).textContent = `statsHP: ${pokemons.statsHP}`;
-    document.querySelector(
-      "#detail-statsattack"
-    ).textContent = `statsattack: ${pokemons.statsAttack}`;
-    document.querySelector(
-      "#detail-statsDefense"
-    ).textContent = `statsDefense: ${pokemons.statsDefence}`;
-    document.querySelector(
-      "#detail-statsSpecielAttack"
-    ).textContent = `statsSpecielAttack: ${pokemons.statsSpecielAttack}`;
-    document.querySelector(
-      "#detail-statsSpecielDefense"
-    ).textContent = `statsSpecielDefense: ${pokemons.statsSpecielDefence}`;
 
     document.querySelector("#dialog-character").showModal();
   }
